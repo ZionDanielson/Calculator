88113b2f1a756613a5a7f25cadd0105abf8d38cf
Zion Danielson <zionsdocs@gmail.com>
2023-04-28 14:04:06 -0400

    index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@
 
 
 //Number TextBoxes-
-const num1          = document.getElementById("numTextbox1");
-const num2          = document.getElementById("numTextbox2");
+const num1          = document.getElementById("num1");
+const num2          = document.getElementById("num2");
 //Math Buttons-
 const addBtn        = document.getElementById("addBtn");
 const subtractBtn   = document.getElementById("subtractBtn");
@@ -17,11 +17,12 @@ const divideBtn     = document.getElementById("divideBtn");
 const answerBtn     = document.getElementById("answerBtn");
 //Answer TextBox-
 const answerTextbox = document.getElementById("answerTextbox")
+
 //More...
-let sum             = (num1 + num2)
-let difference      = (num1 - num2)
-let product         = (num1 * num2)
-let quotient        = (num1 / num2)
+// let sum             = (num1 + num2)
+// let difference      = (num1 - num2)
+// let product         = (num1 * num2)
+// let quotient        = (num1 / num2)
 
 
 
@@ -46,11 +47,9 @@ window.onload = init; // When website loads
 function init() {
 
     addBtn.onclick       = whenaddBtnClicked;        // add Button is clicked
-    subtractBtn.onclick  = whensubtractBtnClicked;   // subtract Button is clicked
-    multiplyBtn.onclick  = whenmultiplyBtnClicked;   // multiply Button is clicked
-    divideBtn.onclick    = whendivideBtnClicked;     // divide Button is clicked
-
-    answerBtn.onclick    = whenanswerBtnClicked;     // answer Button is clicked
+    //subtractBtn.onclick  = whensubtractBtnClicked;   // subtract Button is clicked
+    //multiplyBtn.onclick  = whenmultiplyBtnClicked;   // multiply Button is clicked
+   //divideBtn.onclick    = whendivideBtnClicked;     // divide Button is clicked
 
 }
 
@@ -65,11 +64,16 @@ function init() {
 //   be num1 + num2 after the "answer" button is clicked.
 
 function whenaddBtnClicked() {
+// a.
+    let num1value = parseFloat(num1.value); // This converts the num1 value into a number
+    let num2value = parseFloat(num2.value); // This converts the num2 value into a number
+// b.
+    let sum = (num1value + num2value); // This function calculates the sum of the two number values
+// c.
+    answerTextbox.value = (sum); // This will display the sum/result in the answerTextbox
 
-    let sum = (num1 + num2)
-    
-    console.log(sum)
-
+    console.log(sum) // This will dislay it in the console
+// DONE
 }
 
 //console.log("the index.js has finished executing...");
